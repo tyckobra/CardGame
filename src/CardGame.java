5f9ea820a5244b69337c60db2a7fac3a23408d98
tyckobra <77719515+tyckobra@users.noreply.github.com>
2023-04-20 12:56:50 +0200

    healthpoints

---
+++ src/CardGame.java
@@ -0,0 +1,23 @@
+public class CardGame {
+    int healthpoints;
+
+    public Krigare() {
+        healthpoints = 300;
+    }
+
+    public Trollkarl() {
+        healthpoints = 150;
+    }
+
+    public Bågskytt() {
+        healthpoints = 100;
+    }
+
+    public Tjuv() {
+        healthpoints = 150;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
