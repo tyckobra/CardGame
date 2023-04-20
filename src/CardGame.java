59d6696b7d4359af923bb47893453208541d699c
tyckobra <77719515+tyckobra@users.noreply.github.com>
2023-04-20 13:36:17 +0200

    2023-04-20

--- src/CardGame.java
+++ src/CardGame.java
@@ -1,3 +1,8 @@
+import javax.smartcardio.ATR;
+import javax.smartcardio.Card;
+import javax.smartcardio.CardChannel;
+import javax.smartcardio.CardException;
+
 public class CardGame {
 
     int hälsa;
@@ -10,6 +15,7 @@ public class CardGame {
         styrka = 100;
         pricksäkerhet = 0.8;
         trollkraft = 0;
+        return null;
     }
 
     public String Trollkarl() {
@@ -17,6 +23,7 @@ public class CardGame {
         styrka = 75;
         pricksäkerhet = 0.3;
         trollkraft = 10;
+        return null;
     }
 
     public String Bågskytt() {
@@ -24,6 +31,7 @@ public class CardGame {
         styrka = 50;
         pricksäkerhet = 0.2;
         trollkraft = 0;
+        return null;
     }
 
     public String Tjuv() {
@@ -31,13 +39,14 @@ public class CardGame {
         styrka = 30;
         pricksäkerhet = 1;
         trollkraft = 0;
+        return null;
     }
 
     public String playAgent(String Card) {
         return Card;
     }
 
-    public void getCard() {
+    public void Card() {
         playAgent(Krigare());
         playAgent(Trollkarl());
         playAgent(Bågskytt());
@@ -45,6 +54,46 @@ public class CardGame {
     }
 
     public static void main(String[] args) {
+        Card c = new Card() {
+            @Override
+            public ATR getATR() {
+                return null;
+            }
 
+            @Override
+            public String getProtocol() {
+                return null;
+            }
+
+            @Override
+            public CardChannel getBasicChannel() {
+                return null;
+            }
+
+            @Override
+            public CardChannel openLogicalChannel() throws CardException {
+                return null;
+            }
+
+            @Override
+            public void beginExclusive() throws CardException {
+
+            }
+
+            @Override
+            public void endExclusive() throws CardException {
+
+            }
+
+            @Override
+            public byte[] transmitControlCommand(int i, byte[] bytes) throws CardException {
+                return new byte[0];
+            }
+
+            @Override
+            public void disconnect(boolean b) throws CardException {
+
+            }
+        };
     }
 }
