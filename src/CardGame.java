fab59bd021cca016f9b863ff73189aece3212707
tyckobra <77719515+tyckobra@users.noreply.github.com>
2023-04-21 13:36:33 +0200

    What have i done

--- src/CardGame.java
+++ src/CardGame.java
@@ -5,95 +5,71 @@ import javax.smartcardio.CardException;
 
 public class CardGame {
 
+    private static Object playAgent;
+
     int hälsa;
     int styrka;
     double pricksäkerhet;
     int trollkraft;
 
-    public String Krigare() {
-        hälsa = 300;
-        styrka = 100;
-        pricksäkerhet = 0.8;
-        trollkraft = 0;
-        return null;
+    public CardGame() {
+
+        class Krigare {
+            private final Krigare Krigare;
+
+            public Krigare() {
+                this.Krigare = new Krigare();
+                hälsa = 300;
+                styrka = 100;
+                pricksäkerhet = 0.8;
+                trollkraft = 0;
+            }
+        }
+
+        class Trollkarl {
+            private final Trollkarl Trollkarl;
+
+            public Trollkarl() {
+                this.Trollkarl = new Trollkarl();
+                hälsa = 150;
+                styrka = 75;
+                pricksäkerhet = 0.3;
+                trollkraft = 10;
+            }
+        }
+
+        class Bågskytt {
+            private final Object Bågskytt;
+
+            public Bågskytt() {
+                this.Bågskytt = new Bågskytt();
+                hälsa = 100;
+                styrka = 50;
+                pricksäkerhet = 0.2;
+                trollkraft = 0;
+            }
+        }
+
+        class Tjuv {
+            private final Tjuv Tjuv;
+
+            public Tjuv() {
+                this.Tjuv = new Tjuv();
+                hälsa = 150;
+                styrka = 30;
+                pricksäkerhet = 1;
+                trollkraft = 0;
+            }
+        }
     }
 
-    public String Trollkarl() {
-        hälsa = 150;
-        styrka = 75;
-        pricksäkerhet = 0.3;
-        trollkraft = 10;
-        return null;
-    }
+    public class playAgent {
+        public playAgent() {
+            playAgent = new playAgent();
+        }
 
-    public String Bågskytt() {
-        hälsa = 100;
-        styrka = 50;
-        pricksäkerhet = 0.2;
-        trollkraft = 0;
-        return null;
     }
-
-    public String Tjuv() {
-        hälsa = 150;
-        styrka = 30;
-        pricksäkerhet = 1;
-        trollkraft = 0;
-        return null;
-    }
-
-    public String playAgent(String Card) {
-        return Card;
-    }
-
-    public void Card() {
-        playAgent(Krigare());
-        playAgent(Trollkarl());
-        playAgent(Bågskytt());
-        playAgent(Tjuv());
-    }
-
     public static void main(String[] args) {
-        Card c = new Card() {
-            @Override
-            public ATR getATR() {
-                return null;
-            }
-
-            @Override
-            public String getProtocol() {
-                return null;
-            }
-
-            @Override
-            public CardChannel getBasicChannel() {
-                return null;
-            }
-
-            @Override
-            public CardChannel openLogicalChannel() throws CardException {
-                return null;
-            }
-
-            @Override
-            public void beginExclusive() throws CardException {
-
-            }
-
-            @Override
-            public void endExclusive() throws CardException {
-
-            }
-
-            @Override
-            public byte[] transmitControlCommand(int i, byte[] bytes) throws CardException {
-                return new byte[0];
-            }
-
-            @Override
-            public void disconnect(boolean b) throws CardException {
-
-            }
-        };
+        System.out.println(playAgent);
     }
 }
